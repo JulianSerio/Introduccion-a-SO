1f7c14d833922f0649e11d1bea6fb18665416705
Julián Serio <83135918+JulianSerio@users.noreply.github.com>
2023-09-28 15:13:29 -0300

    Update ejercicio25.sh

--- shell-script/ejercicio25.sh
+++ shell-script/ejercicio25.sh
@@ -1,1 +1,40 @@
-for user in users 
+#!/bin/bash
+
+print(){
+	for user in ${users[@]}; do
+		echo "$user"
+	done
+}
+compruebo(){
+	
+	case "$1" in
+	b) 
+		pos=$2
+		if [ pos -le ${#users[@]} ] && [ pos -ge 0 ]; then
+			echo "$users[$pos]"
+		else
+			echo "Esa pos no existe"
+		fi
+		;;
+	-l)
+		long=${#users[@]}
+		echo "$long"
+		;;
+	-i)
+		print
+		;;
+	*)
+		echo "error!"
+		;;
+	esac
+}		
+
+
+users=()
+for user in /home/*; do 
+	nombre=$(basename $user)
+	users+=($nombre)
+done
+print
+echo ""
+compruebo
